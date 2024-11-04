a739c5db55bf47c77aade6d9289a73e7c125f2ff
Yellowflash28 <114740900+Yellowflash28@users.noreply.github.com>
2024-11-04 15:42:56 +0530

    d

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {…………………} from 'react-router-dom'; 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <>
+
+
+            // fill the routing part
+
+
+    </>
+  );
+}
+
+export default App;
